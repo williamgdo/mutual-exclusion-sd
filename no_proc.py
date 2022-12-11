876b3951a44d54d0e20db5c7c19f21f73fd8c506
Eetrexx <j.g3412@gmail.com>
2022-12-11 16:43:01 -0300

    stuff '

--- no_proc.py
+++ no_proc.py
@@ -4,8 +4,7 @@
 
 import socket, sys, struct, threading, pickle, time, random
 
-multicast_group = '225.0.0.1'    	# configura o ip hospedeiro para a conexao
-multicast_port = 2022           # porta de comunicacao (a mesma que a do servidor)
+multicast_group = '225.0.0.1'    
 
 def create_multicast():
 
@@ -18,41 +17,74 @@ def create_multicast():
     return sock
 
 
-def send(s_t, msg):
-    time.sleep(random.randint(3, 10))
-    s_t.sendto(pickle.dumps(msg), (multicast_group, multicast_port))
+def send_req(s_t, msg):
+    s_t.sendto(pickle.dumps(msg), (multicast_group, 2020)
+    s_t.sendto(pickle.dumps(msg), (multicast_group, 2021)
+    s_t.sendto(pickle.dumps(msg), (multicast_group, 2022)
 
+def send(s_t, msg, port):
+    s_t.sendto(pickle.dumps(msg), (multicast_group, port))
+
+
+                
 def listen (p_id, msg_list, clock):
 
     s_t = create_multicast()
 
     while True:
         
-        time.sleep(random.randint(3, 10))
 
         msg = pickle.loads(s_t.recv( 1024 ))
+
+        prev_clock = (clock[0], p_id)
        
         if(msg[1] > clock[0]):
             clock[0] = msg[1]+1
         else:
             clock[0] = clock[0]+1
 
+        print("Mensagem: ", msg)
+
         if(msg[3] == True):
-            print("Mensagem: ", msg)
             msg_list.append(msg)
 
             msg_list.sort(key = lambda x: (x[1], x[2]))
             
-            time.sleep(3)
             print("Lista de msgs: ----->")
             print(msg_list)
             print("-------------------->")
-            ack = ["ACK", clock[0], p_id, False]
-            send(s_t, ack)
-        else:
-            print("ACK: ", msg)
 
-def Main():
+       
+        
+        if (rec_want[0] == True):
+
+                ack_counter = 0
+                while ack_counter < 2:
+                    ack = pickle.loads(s_t.recv( 1024 ))
+                    if (ack[3] == False):
+                        ack_counter += 1
+
+                port_to_use = "202" + str(msg[2])
+                ack = ["ACK", clock[0], p_id, False]
+                send(s_t, ack, port_to_use)
+        else: 
+                ack = ["ACK", clock[0], p_id, False]
+                if(p_id == 0):
+                    port1 = "2021"
+                    port2 = "2022"
+                elif (p_id == 1):
+                    port1 = "2020"
+                    port2 = "2022"
+                else:
+                    port1 = "2020"
+                    port2 = "2021"
+                send(s_t, ack, port1)
+                send(s_t, ack, port2)
+            
+
+def Main(argv):
+
+    global multicast_port = argv[0] 
 
     s_t = create_multicast()
 
@@ -64,7 +96,8 @@ def Main():
 
     p_id = int(p_id)
     clock = [int(clock)]
-
+    
+    rec_want = [False]
 
     t_proc = threading.Thread(target=listen, args=(p_id, msg_list, clock))
 
@@ -72,13 +105,15 @@ def Main():
 
     while True:
         input()
-        msg = input("Mensagem: ")
-        msg = [msg, clock[0]+1, p_id, True]
-        send(s_t, msg)
-        print(msg)
+        msg = input("Usar recurso? (s/n): ")
+        if msg == "s":
+            rec_want[0] = True
+            msg = ["REC WANT", clock[0]+1, p_id, True]
+            send_rec(s_t, msg)
+            print(msg)
 
 
 
 
 if __name__ == '__main__':
-    Main()
+    Main(sys.argv)
